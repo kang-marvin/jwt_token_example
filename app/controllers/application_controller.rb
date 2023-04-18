928bebc4aaa764ba0ee371889d22880d80e74aac
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-04-18 22:04:28 +0300

    Add authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  include JwtToken
+
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last if auth_header
+
+    begin
+      decoded = JwtToken.decode(token)
+      @current_user = User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: 401
+    rescue JWT:DecodeError => e
+      render json: { errors: e.message }, status: 401
+    end
+  end
 end
