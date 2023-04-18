01ae460ba04240b1e621bde3d7ee75fef452e9aa
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-04-18 22:14:04 +0300

    Add login function

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationController < ApplicationController
+
+  skip_before_action :authenticate_user
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      token = JwtToken.encode({user_id: @user.id})
+      render json: { token: token, username: @user&.user_name }, status: 200
+    else
+      render json: { error: 'unauthorized' }, status: 401
+    end
+  end
+end
--- app/controllers/concerns/jwt_token.rb
+++ app/controllers/concerns/jwt_token.rb
@@ -6,7 +6,7 @@ module JwtToken
 
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  def jwt_encode(payload, expiration: 7.days.from_now)
+  def jwt_encode(payload, expiration: 1.minute.from_now)
     payload[:exp] = expiration.to_i
     JWT.encode(payload, SECRET_KEY)
   end
